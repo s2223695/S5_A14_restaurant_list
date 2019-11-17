f43c45bbe332288ec64747c43edaf96a99d57359
yenting_liu <s2223695@yahoo.com.tw>
2019-11-17 21:35:35 +0800

    Finish index and action and page, add new page and create feature, install body-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // Setting database connection
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,6 +27,9 @@ const restList = require('./models/seeds/restaurant.json')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Setting body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // Define server variables
 const port = 3000
 
@@ -34,7 +38,10 @@ app.use(express.static('public'))
 
 // Setting routes
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restList.results })
+  Restaurant.find((err, restaurants) => {
+    if (err) return console.log(err)
+    res.render('index', { restaurants })
+  })
 })
 
 app.get('/restaurants', (req, res) => {
@@ -42,11 +49,15 @@ app.get('/restaurants', (req, res) => {
 })
 
 app.get('/restaurants/new', (req, res) => {
-  res.send('page for add new restaurant')
+  res.render('new')
 })
 
 app.post('/restaurants/new', (req, res) => {
-  res.send('add new restaurant')
+  const restaurant = new Restaurant(req.body)
+  restaurant.save(err => {
+    if (err) return console.log(err)
+    res.redirect('/')
+  })
 })
 
 app.get('/restaurants/:id', (req, res) => {
