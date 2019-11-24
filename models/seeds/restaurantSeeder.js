bbbfb24bba3acb19b7b78bb8407ee4cfa6c02e3d
yenting_liu <s2223695@yahoo.com.tw>
2019-11-24 14:15:46 +0800

    Add bcrypt to user registration route

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const User = require('../user')
+const bcrypt = require('bcryptjs')
 
 // Setting database connection
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -18,42 +19,35 @@ db.once('open', () => {
   const restaurants = require('./restaurant.json').results
 
   users.forEach(user => {
-    // bcrypt.genSalt(10, (err, salt) => {
-    //   bcrypt.hash(users[i].password, salt, (err, hash) => {
-    //     const newUser = new User({
-    //       name: users[i].name,
-    //       email: users[i].email,
-    //       password: hash
-    //     })
-    const newUser = new User({
-      name: user.name,
-      email: user.email,
-      password: user.password
-    })
-    newUser.save().then(user => {
-      // create restaurant data for each user
-      const userRestaurants = restaurants.splice(0, 3)
-      userRestaurants.forEach(restaurant => {
-        Restaurant.create({
-          name: restaurant.name,
-          name_en: restaurant.name_en,
-          category: restaurant.category,
-          image: restaurant.image,
-          location: restaurant.location,
-          phone: restaurant.phone,
-          google_map: restaurant.google_map,
-          rating: restaurant.rating,
-          description: restaurant.description,
-          userId: user._id
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        const newUser = new User({
+          name: user.name,
+          email: user.email,
+          password: hash
+        })
+        newUser.save().then(user => {
+          // create restaurant data for each user
+          const userRestaurants = restaurants.splice(0, 3)
+          userRestaurants.forEach(restaurant => {
+            Restaurant.create({
+              name: restaurant.name,
+              name_en: restaurant.name_en,
+              category: restaurant.category,
+              image: restaurant.image,
+              location: restaurant.location,
+              phone: restaurant.phone,
+              google_map: restaurant.google_map,
+              rating: restaurant.rating,
+              description: restaurant.description,
+              userId: user._id
+            })
+          })
+        }).catch(err => {
+          console.log(err)
         })
       })
-    }).catch(err => {
-      console.log(err)
     })
-    //     })
-    //   })
-    // })
-
     console.log('done')
   })
 })
