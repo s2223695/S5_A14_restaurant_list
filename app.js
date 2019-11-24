d8c44d3d4efe18eaa069c3a6f6c09bfa9540450b
yenting_liu <s2223695@yahoo.com.tw>
2019-11-24 15:28:33 +0800

    Add facebook login functionality and remove fb secret key

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 // Setting database connection
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
@@ -62,6 +66,7 @@ app.use(express.static('public'))
 app.use('/', require('./routes/home'))
 app.use('/restaurants', require('./routes/restaurant'))
 app.use('/users', require('./routes/user'))
+app.use('/auth', require('./routes/auths'))
 
 // Start and listen on the Express server
 app.listen(port, () => {
