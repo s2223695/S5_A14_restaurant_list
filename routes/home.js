d76678865882fdc673e9665fe1ca5dd3c82f422a
yenting_liu <s2223695@yahoo.com.tw>
2019-11-24 16:32:47 +0800

    Make sort and search coworkable

--- routes/home.js
+++ routes/home.js
@@ -13,34 +13,24 @@ router.get('/', authenticated, (req, res) => {
 
 router.get('/search', authenticated, (req, res) => {
   let keyword = req.query.keyword
+  let sortType = req.query.sort
 
   if (keyword === undefined) {
     keyword = ''
   }
-
-  Restaurant.find({ userId: req.user._id }, (err, restaurants) => {
-    if (err) console.log(err)
-    const filteredRestaurants = restaurants.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.toLowerCase().includes(keyword.toLowerCase()))
-
-    res.render('index', { restaurants: filteredRestaurants, keyword })
-  })
-})
-
-router.get('/:sort', authenticated, (req, res) => {
-  let keyword = req.query.keyword
-
-  if (keyword === undefined) {
-    keyword = ''
+  if (sortType === undefined) {
+    sortType = 'name'
   }
 
-  let sortType = {}
-  sortType[req.params.sort] = 'asc'
+  let sort = {}
+  sort[sortType] = 'asc'
 
   Restaurant.find({ userId: req.user._id })
-    .sort(sortType)
+    .sort(sort)
     .exec((err, restaurants) => {
-      if (err) return console.error(err)
-      return res.render('index', { restaurants })
+      if (err) console.log(err)
+      const filteredRestaurants = restaurants.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.toLowerCase().includes(keyword.toLowerCase()))
+      res.render('index', { restaurants: filteredRestaurants, keyword })
     })
 })
 
