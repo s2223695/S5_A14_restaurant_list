d29f2d7f049cc9be65966574e9d772208b162a12
yenting_liu <s2223695@yahoo.com.tw>
2019-11-24 18:40:23 +0800

    Add data check for Edit and New page

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -15,11 +15,36 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/new', (req, res) => {
-  const restaurant = new Restaurant(req.body)
-  restaurant.save(err => {
-    if (err) return console.log(err)
-    res.redirect('/')
-  })
+  const userInput = req.body
+  let errors = []
+  if (!userInput.name) {
+    errors.push({ message: '請輸入名稱' })
+  }
+  if (isNaN(userInput.rating)) {
+    errors.push({ message: '評分須為數字' })
+  }
+
+  if (errors.length > 0) {
+    res.render('new', {
+      errors,
+      name: userInput.name,
+      name_en: userInput.name_en,
+      category: userInput.category,
+      image: userInput.image,
+      location: userInput.location,
+      phone: userInput.phone,
+      google_map: userInput.google_map,
+      rating: userInput.rating,
+      description: userInput.description
+    })
+  } else {
+    const restaurant = new Restaurant(req.body)
+    restaurant.userId = req.user._id
+    restaurant.save(err => {
+      if (err) return console.log(err)
+      res.redirect('/')
+    })
+  }
 })
 
 router.get('/:id', (req, res) => {
@@ -37,14 +62,32 @@ router.get('/:id/edit', (req, res) => {
 })
 
 router.put('/:id/edit', (req, res) => {
-  Restaurant.findById({ _id: req.params.id, userId: req.user._id }, (err, restaurant) => {
-    if (err) return console.log(err)
-    Object.assign(restaurant, req.body)
-    restaurant.save(err => {
-      if (err) return console.log(err)
-      res.redirect(`/restaurants/${restaurant.id}`)
+  const userInput = req.body
+  userInput.id = req.params.id
+  let errors = []
+  if (!userInput.name) {
+    errors.push({ message: '請輸入名稱' })
+  }
+  if (isNaN(userInput.rating)) {
+    errors.push({ message: '評分須為數字' })
+  }
+
+  if (errors.length > 0) {
+    res.render('edit', {
+      errors,
+      restaurant: userInput
     })
-  })
+  } else {
+    console.log(req.params.id)
+    Restaurant.findById({ _id: req.params.id, userId: req.user._id }, (err, restaurant) => {
+      if (err) return console.log(err)
+      Object.assign(restaurant, req.body)
+      restaurant.save(err => {
+        if (err) return console.log(err)
+        res.redirect(`/restaurants/${restaurant.id}`)
+      })
+    })
+  }
 })
 
 router.delete('/:id/delete', (req, res) => {
